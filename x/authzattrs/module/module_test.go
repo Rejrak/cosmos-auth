3b182be274f596cc0d396fd830f9e75760017c15
Rejrak <lucchettiroberto95@outlook.com>
2026-09-19 11:42:22 +0200

    chore(authzattrs): seed local demo issuer registry

--- x/authzattrs/module/module_test.go
+++ x/authzattrs/module/module_test.go
@@ -17,3 +17,20 @@ func TestDefaultAndValidateGenesis(t *testing.T) {
 	require.NoError(t, module.ValidateGenesis(encCfg.Codec, encCfg.TxConfig, module.DefaultGenesis(encCfg.Codec)))
 	require.Error(t, module.ValidateGenesis(encCfg.Codec, encCfg.TxConfig, []byte("{")))
 }
+
+func TestValidateDemoIssuerGenesis(t *testing.T) {
+	encCfg := moduletestutil.MakeTestEncodingConfig(authzattrs.AppModule{})
+	module := authzattrs.NewAppModule(encCfg.Codec, keeper.Keeper{})
+	genesis := []byte(`{
+		"params": {},
+		"authorizations": [],
+		"issuer_sets": [{"issuer_set_id":"9","active":true,"policy_id":"policy-bank-send","msg_type_url":"/cosmos.bank.v1beta1.MsgSend","threshold_weight":"5"}],
+		"issuers": [
+			{"issuer_set_id":"9","issuer_id":"issuer-alpha","key_type":"ISSUER_KEY_TYPE_ED25519","public_key":"A6EHv/POEL4dcN0Y50vAmWfk1jCbpQ1fHdyGZBJVMbg=","weight":"2","active":true,"valid_from_height":"1","valid_until_height":"9223372036854775807"},
+			{"issuer_set_id":"9","issuer_id":"issuer-beta","key_type":"ISSUER_KEY_TYPE_ED25519","public_key":"Kay64UG8yvCyLhqU000LxzYeUm0L/hLIl5S8kyKWbdc=","weight":"3","active":true,"valid_from_height":"1","valid_until_height":"9223372036854775807"}
+		],
+		"current_issuer_sets": [{"policy_id":"policy-bank-send","msg_type_url":"/cosmos.bank.v1beta1.MsgSend","issuer_set_id":"9"}],
+		"last_applied_batch_ids": []
+	}`)
+	require.NoError(t, module.ValidateGenesis(encCfg.Codec, encCfg.TxConfig, genesis))
+}
